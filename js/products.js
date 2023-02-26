33c4233f3cd5a99d804ad334d6262cec2e51d6fb
eduard Ivanov <eduardivanov803@gmail.com>
2023-02-26 16:01:23 +0200

    update

--- js/products.js
+++ js/products.js
@@ -61,43 +61,6 @@ const products = [
     kg1: "0.4kg",
     cantitate: 1,
   },
-
-  const products = [
-    {
-      id: 1,
-      categoryId: 4,
-      home: true,
-      descriere: "Biscuiți din aluat fraged, cu cacao pudră și umplutură de lapte condensat, acoperiți cu glazură neagră de cofetărie",
-      image: "dame_in_ciocolata.png",
-      image1: "dame_in_ciocolata.png",
-      name: "Dame în Ciocolată",
-      pret1: "40.00",
-      kg1: "0.5kg",
-      cantitate: 1,
-    },
-    {
-      id: 2,
-      categoryId: 5,
-      descriere: "Biscuiți din aluat fărimicios cu umplutură de lapte condensat",
-      image: "nuci.png",
-      image1: "nuci1.png",
-      name: "Nuci cu Lapte Condensat",
-      pret1: "53.00",
-      kg1: "0.6kg",
-      cantitate: 1,
-    },
-    {
-      id: 3,
-      categoryId: 3,
-      descriere: "Fursec din aluat fraged cu umplutură de zmeură și fulgi de cocos",
-      image: "Paleuri cu zmeura.png",
-      image1: "Paleuri cu zmeura.png",
-      name: "Paleuri cu Magiun de Zmeură și Fulgi de Cocos",
-      pret1: "28.50",
-      kg1: "0.4kg",
-      cantitate: 1,
-    },
->>>>>>> 87c93ded290c8af741ccb13d800262aa6f714efc
   {
     id: 4,
     categoryId: 1,
